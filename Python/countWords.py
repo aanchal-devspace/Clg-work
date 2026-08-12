7f73589db6a976c482567027bfe33e6067fdc91f
aanchal <259937850+aanchal-devspace@users.noreply.github.com>
2026-08-12 23:46:14 +0530

    string operations added

---
+++ Python/countWords.py
@@ -0,0 +1,6 @@
+text="Python is a Programming Language."
+count=0
+for ch in text:
+    if ch==" ":
+        count+=1
+print("Number of words is : ",count+1)
---
+++ Python/removeSpace.py
@@ -0,0 +1,6 @@
+text="Python is a Programming Language"
+for ch in text:
+    if ch==" ":
+        continue
+    else:
+        print(ch,end="")
