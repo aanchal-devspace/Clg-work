352df3943d87db5aeb10a0c82eddf13a8d760f2f
aanchal <259937850+aanchal-devspace@users.noreply.github.com>
2026-08-19 01:14:10 +0530

    LIST OPERATIONS

--- Python/ListFrequency.py
+++ Python/ListFrequency.py
@@ -1,1 +1,10 @@
 print("AANCHAL")
+num=[25,78,90,57,25,78,25,86]
+num.sort()
+count=1
+for i in range(len(num)):
+    if(num[i-1]==num[i]):
+        count+=1
+    else:
+        print(f"count of {num[i-1]} is : {count} ")
+        count=1
